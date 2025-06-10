fc2e3b0bf6c840e47ca34fef0e6b9e0e8a1ca9e2
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-06-10 10:43:49 +1000

    just let Dagster find assets

--- example/definitions.py
+++ example/definitions.py
@@ -3,14 +3,13 @@ from pathlib import Path
 import dagster as dg
 
 from example import assets
-from example.assets import products, sales_data, sales_reps
 from example.dagster_csv import PolarsCsvIOManager
 
-assets = dg.load_assets_from_modules([assets])
+example_assets = dg.load_assets_from_modules([assets])
 
 fixtures_path = str(Path(Path(__file__).parent, "..", "tests", "fixtures"))
 
 defs = dg.Definitions(
-    assets=[sales_data, products, sales_reps],
+    assets=example_assets,
     resources={"polars_csv_io_manager": PolarsCsvIOManager(base_dir=fixtures_path)},
 )
