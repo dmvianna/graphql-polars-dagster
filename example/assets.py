054faf4a2667e557fcffb01c1365ed01063884a5
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-06-10 14:24:36 +1000

    use environment variable to define root directory for assets

--- example/assets.py
+++ example/assets.py
@@ -2,25 +2,27 @@ from pathlib import Path
 
 import dagster as dg
 import polars as pl
+from upath import UPath
 
-fixtures_path = Path(Path(__file__).parent, "..", "tests", "fixtures")
+default_root_path = Path(Path(__file__).parent, "..", "tests", "fixtures").__str__()
+root_path: UPath = UPath(dg.EnvVar("ROOT_PATH").get_value(default_root_path))
 
 
 @dg.asset
 def reps_csv() -> pl.LazyFrame:
-    return pl.scan_csv(fixtures_path / Path("sales_reps.csv"))
+    return pl.scan_csv(root_path / Path("sales_reps.csv"))
 
 
 @dg.asset
 def sales_csv() -> pl.LazyFrame:
-    return pl.scan_csv(fixtures_path / Path("sales_data.csv"))
+    return pl.scan_csv(root_path / Path("sales_data.csv"))
 
 
 @dg.asset
 def products_csv() -> pl.LazyFrame:
-    return pl.scan_csv(fixtures_path / Path("products.csv"))
+    return pl.scan_csv(root_path / Path("products.csv"))
 
 
 @dg.asset
 def sales_sink(sales_csv: pl.LazyFrame) -> None:
-    return sales_csv.sink_csv(fixtures_path / Path("sales_sink.csv"))
+    return sales_csv.sink_csv(root_path / Path("sales_sink.csv"))
--- example/definitions.py
+++ example/definitions.py
@@ -1,7 +1,7 @@
 import dagster as dg
 
 from example import assets
-from example.assets import fixtures_path
+from example.assets import root_path
 from example.dagster_csv import LazyCsvManager
 
 example_assets = dg.load_assets_from_modules([assets])
@@ -10,6 +10,6 @@ example_assets = dg.load_assets_from_modules([assets])
 defs = dg.Definitions(
     assets=example_assets,
     resources={
-        "lazy_csv_manager": LazyCsvManager(root_path=str(fixtures_path)),
+        "lazy_csv_manager": LazyCsvManager(root_path=str(root_path)),
     },
 )
