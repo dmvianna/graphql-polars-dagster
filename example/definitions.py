c041d0adcf1a395237ba5fe67398603d360837ea
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2025-06-11 14:57:30 +1000

    add some jobs

--- example/definitions.py
+++ example/definitions.py
@@ -6,9 +6,13 @@ from example.dagster_csv import LazyCsvManager
 
 example_assets = dg.load_assets_from_modules([assets])
 
+all_assets_jobs = dg.define_asset_job(name="all_assets_job")
+
+products_job = dg.define_asset_job(name="products_job", selection="products_csv")
 
 defs = dg.Definitions(
     assets=example_assets,
+    jobs=[all_assets_jobs, products_job],
     resources={
         "io_manager": LazyCsvManager(root_path=str(root_path)),
     },
